0790c9411e1cc73dc23a5e308ecc3bccb4fd7b52
GabrielDainez <145864729+GabrielDainez@users.noreply.github.com>
2023-11-29 15:45:24 -0300

    sds

--- gabriel.js
+++ gabriel.js
@@ -23,10 +23,33 @@ ScrollReveal({
 });
 
 
-ScrollReveal().reveal('.content, .titulo, .titulo-habilidades, .titulo-portfolio, .titulo-contato' , { origin: 'top'});
+ScrollReveal().reveal('.titulo, .titulo-habilidades, .titulo-portfolio, .titulo-contato' , { origin: 'top'});
 
 
 ScrollReveal().reveal('.skill-content, .portfolio-content, form' , { origin: 'bottom'});
 
 ScrollReveal().reveal('.foto' , { origin: 'left'});
 
+/*========================links=========================*/
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul li a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav ul li a[href*=' + id +']').classList.add('active');
+            })
+
+        }
+    })
+}
+
+AOS.init();
